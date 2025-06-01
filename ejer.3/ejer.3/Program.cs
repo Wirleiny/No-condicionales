bb343a8ca7ed7092a6a2f9977bdd2191d9d8af60
Wirleiny <wirleinycruzpolanco@gmail.com>
2025-06-01 18:37:39 -0400

    Add files via upload
    Este programa solicita tres números enteros y muestra el resultado de sumarlos, restarlos, multiplicarlos y dividir el primero entre los otros dos.

---
+++ ejer.3/ejer.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Introduce el primer número entero:");
+        int numero1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Introduce el segundo número entero:");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Introduce el tercer número entero:");
+        int numero3 = int.Parse(Console.ReadLine());
+
+        int suma = numero1 + numero2 + numero3;
+        int resta = numero1 - numero2 - numero3;
+        int multiplicacion = numero1 * numero2 * numero3;
+        double division = (double)numero1 / numero2 / numero3;
+
+        Console.WriteLine("Suma: " + suma);
+        Console.WriteLine("Resta: " + resta);
+        Console.WriteLine("Multiplicación: " + multiplicacion);
+        Console.WriteLine("División: " + division);
+    }
+}
