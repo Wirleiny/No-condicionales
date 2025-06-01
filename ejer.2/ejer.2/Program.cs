b912c2bb37cec7e1139c5c25ca5278b02f83801e
Wirleiny <wirleinycruzpolanco@gmail.com>
2025-06-01 18:38:21 -0400

    Add files via upload
    Este programa calcula el sueldo neto de un empleado, restando los descuentos de AFP y SFS al sueldo bruto según el pago por hora y horas trabajadas.

---
+++ ejer.2/ejer.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Introduce el pago por hora:");
+        double pagoPorHora = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Introduce las horas trabajadas:");
+        double horasTrabajadas = double.Parse(Console.ReadLine());
+
+        double sueldoBruto = pagoPorHora * horasTrabajadas;
+
+        double descuentoAFP = sueldoBruto * 0.0287;
+        double descuentoSFS = sueldoBruto * 0.0304;
+
+        double totalDescuentos = descuentoAFP + descuentoSFS;
+
+        double sueldoNeto = sueldoBruto - totalDescuentos;
+
+        Console.WriteLine("Sueldo Bruto: " + sueldoBruto);
+        Console.WriteLine("Descuento AFP: " + descuentoAFP);
+        Console.WriteLine("Descuento SFS: " + descuentoSFS);
+        Console.WriteLine("Total Descuentos: " + totalDescuentos);
+        Console.WriteLine("Sueldo Neto: " + sueldoNeto);
+    }
+}
