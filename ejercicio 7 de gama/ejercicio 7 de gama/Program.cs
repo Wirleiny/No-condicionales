9eb4631dfa7545e29759a4e017f023ee5c656dd0
Wirleiny <wirleinycruzpolanco@gmail.com>
2025-06-01 18:29:06 -0400

    Add files via upload
    Este ejercicio calcula el nuevo sueldo de un empleado aplicando un aumento del 15% al sueldo ingresado por el usuario, y muestra ambos sueldos en formato de moneda.

---
+++ ejercicio 7 de gama/ejercicio 7 de gama/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ejercicio_7_de_gama
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduce el sueldo del empleado: ");
+            double sueldoAnterior = double.Parse(Console.ReadLine());
+
+            double aumento = sueldoAnterior * 0.15;
+
+            double sueldoNuevo = sueldoAnterior + aumento;
+             
+            Console.WriteLine($"Sueldo anterior: {sueldoAnterior:C2}");
+            Console.WriteLine($"Nuevo sueldo (con aumento del 15%): {sueldoNuevo:C2}");
+
+        }
+    }
+}
