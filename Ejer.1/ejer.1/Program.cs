ee6eebb80863d4a5d7de0ad177e7fbba60ab914a
Wirleiny <wirleinycruzpolanco@gmail.com>
2025-06-01 18:38:51 -0400

    Add files via upload
    Este programa pide dos números enteros al usuario y muestra su suma, resta, multiplicación y división.

---
+++ Ejer.1/ejer.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Introduce el primer número entero:");
+        int numero1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Introduce el segundo número entero:");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        int suma = numero1 + numero2;
+        int resta = numero1 - numero2;
+        int multiplicacion = numero1 * numero2;
+        double division = (double)numero1 / numero2; // el (double) lo que hace es pasar la variable de tipo INT a DOUBLE
+
+        Console.WriteLine("Suma: " + suma);
+        Console.WriteLine("Resta: " + resta);
+        Console.WriteLine("Multiplicación: " + multiplicacion);
+        Console.WriteLine("División: " + division);
+    }
+}
