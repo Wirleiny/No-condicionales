395534ae4aee7564a4fb9d6fab6e3517c485820c
Wirleiny <wirleinycruzpolanco@gmail.com>
2025-06-01 18:34:38 -0400

    Add files via upload
    Este ejercicio calcula el importe total de una compra multiplicando la cantidad de artículos por su precio, y muestra el resultado.

---
+++ ejer.5/ejer.5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Introduce la cantidad del artículo:");
+        int cantidad = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Introduce el precio del artículo:");
+        double precio = double.Parse(Console.ReadLine());
+
+        double importe = cantidad * precio;
+
+        Console.WriteLine("El importe total es: " + importe);
+    }
+}
